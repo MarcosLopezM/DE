b374cfc2ff1923c8c969c61ab84f59c47aff8965
Marcos LM <marcoslm@ciencias.unam.mx>
2026-07-11 19:37:18 -0600

    Add data Transformation step

---
+++ weather-etl-csv/transform.py
@@ -0,0 +1,48 @@
+import datetime as dt
+
+import polars as pl
+
+from extract import get_json
+
+
+def get_data(city_name: list[str] | str, start_date: dt.date, end_date: dt.date):
+    if isinstance(city_name, str):
+        city_name = [city_name]
+
+    weather_data = [get_json(city, start_date, end_date) for city in city_name]
+    return dict(zip(city_name, weather_data))
+
+
+def transform_weather_data(raw_data: dict) -> tuple[pl.DataFrame, pl.DataFrame]:
+    hourly_dfs = [
+        pl.DataFrame(data["hourly"]).with_columns(city=pl.lit(city))
+        for city, data in raw_data.items()
+        if data.get("hourly")
+    ]
+    daily_dfs = [
+        pl.DataFrame(data["daily"]).with_columns(city=pl.lit(city))
+        for city, data in raw_data.items()
+        if data.get("daily")
+    ]
+
+    hourly_df = pl.concat(hourly_dfs) if hourly_dfs else pl.DataFrame
+    daily_df = pl.concat(daily_dfs) if daily_dfs else pl.DataFrame
+
+    if not hourly_df.is_empty():
+        hourly_df = hourly_df.with_columns(
+            pl.col("temperature_2m").forward_fill(),
+            pl.col("rain").fill_null(0.0),
+            time=pl.col("time").str.to_datetime(),
+        )
+
+    if not daily_df.is_empty():
+        daily_df = daily_df.with_columns(time=pl.col("time").str.to_date())
+
+    return hourly_df, daily_df
+
+
+raw_data = get_data("Mexico City", dt.date(2026, 7, 1), dt.date(2026, 7, 5))
+h_df, d_df = transform_weather_data(raw_data)
+print(h_df.head())
+print("*" * 50)
+print(d_df.head())
