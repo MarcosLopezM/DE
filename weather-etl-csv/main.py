597bb9d15b0aa4662492adb43e820bbfd0c2c3e8
Marcos LM <marcoslm@ciencias.unam.mx>
2026-07-11 20:58:13 -0600

    Add complete ETL pipeline

---
+++ weather-etl-csv/main.py
@@ -0,0 +1,23 @@
+import datetime as dt
+from transform import transform_weather_data, get_data
+from load import load_db
+
+
+def main():
+    cities = ["Mexico City", "Ozumba de Alzate", "Morelos", "Veracruz"]
+    start_date = dt.date(2026, 1, 1)
+    end_date = dt.date(2026, 7, 6)
+
+    print("Extrancting the data from API...")
+    data = get_data(cities, start_date, end_date)
+
+    print("Transforming data...")
+    hourly_df, daily_df = transform_weather_data(data)
+
+    load_db(hourly_df, daily_df)
+
+    print("ETL Pipeline completed successfully!")
+
+
+if __name__ == "__main__":
+    main()
