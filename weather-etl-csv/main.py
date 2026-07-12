462f8f48aaaeda98422c1940cfe4d6b70a62ae12
Marcos LM <marcoslm@ciencias.unam.mx>
2026-07-11 21:52:36 -0600

    Change time range

--- weather-etl-csv/main.py
+++ weather-etl-csv/main.py
@@ -6,7 +6,7 @@ from load import load_db
 def main():
     cities = ["Mexico City", "Ozumba de Alzate", "Morelos", "Veracruz"]
     start_date = dt.date(2026, 1, 1)
-    end_date = dt.date(2026, 7, 6)
+    end_date = dt.date(2026, 7, 11)
 
     print("Extrancting the data from API...")
     data = get_data(cities, start_date, end_date)
